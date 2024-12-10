8660010dc184537799007d413cfc4b667114d20b
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-12-10 09:44:18 +0100

    Events A

--- src/components/theory/Events.jsx
+++ src/components/theory/Events.jsx
@@ -1,8 +1,30 @@
+
+/**
+ * La gestion des events du DOM ne se fait que sur des élement du DOM
+ * Cette gestion se fait pvia l'appel fonctions lorsqu'un événement se produit.
+ * Ces fonctions sont appelées des gestionnaires d'événements.
+ * Une convention est de les préfixer de "handle" suivi du nom de l'événement.
+ * L'association se fait la la version camelaCase de l'attribut d'événement. 
+ * 
+ * @example:
+ * 
+ * Evenement "click" => attibut HTML standard "onclick"  => En react "onClick"
+ * Association <button onClick={handleClick}>Click Here</button>
+ */
 function Events(){
+
+    const handleClick = (event) => {
+        console.log('Button clicked', event);
+    }
     return(
         <>
+            <Component onClick={handleClick}></Component>
+            <button onClick={handleClick}>Click Here</button>
         </>
     )
 }
 
+const Component = () => <button>Click Here But Nothing will happen</button>
+
 export default Events;
+
