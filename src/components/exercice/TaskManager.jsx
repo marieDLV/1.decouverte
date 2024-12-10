af609e89c450e70091fd8c35365928a8e38f41b2
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-12-10 15:15:37 +0100

    Implémentation des bonus

--- src/components/exercice/TaskManager.jsx
+++ src/components/exercice/TaskManager.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
-
-
 function TaskManager() {
 
     const [tasks, setTasks] = useState([]);
     const [taskDescrition, setTaskDescription] = useState("");
+    const [errorMessage, setErrorMessage ] = useState("");
 
     const handleTaskDescriptionChange = (event) => {
         setTaskDescription(event.target.value);
     }
 
     const handleAddTask = () => {
+        if(!taskDescrition) {
+            setErrorMessage("Veuillez saisir une tâche.");
+            return;
+        }
+        setErrorMessage("");
         setTasks([taskDescrition, ...tasks /* ... (spread - itération) */]);
         setTaskDescription("");
     }
@@ -31,6 +35,9 @@ function TaskManager() {
                 onChange={handleTaskDescriptionChange}
             />
             <button onClick={handleAddTask}>Ajouter une tâche</button>
+            {
+                errorMessage && <p style={{color: "red"}}>{errorMessage}</p>
+            }
             <hr />
             {
                 !tasks.length
@@ -38,18 +45,21 @@ function TaskManager() {
                     (<p>Aucune tâche pour le moment.</p>)
                     :
                     (
-                        <ol>
-                            {
-                                tasks.map((task, index) => {
-                                    return (
-                                        <li key={index}>
-                                            <span>{task}</span>
-                                            <button onClick={() => handleDeleteTask(task)}>Supprimer</button>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ol>
+                        <>
+                            <em>Tâches courantes {tasks.length}</em>
+                            <ol>
+                                {
+                                    tasks.map((task, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <span>{task}</span>
+                                                <button onClick={() => handleDeleteTask(task)}>Supprimer</button>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ol>
+                        </>
                     )
             }
         </fieldset>
