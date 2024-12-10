a79d25a42816d6291b4f0e1fd7e1aafafa2c5d31
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-12-10 14:28:34 +0100

    Première étape logique

--- src/components/exercice/TaskManager.jsx
+++ src/components/exercice/TaskManager.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 
 
 
 function TaskManager(){
 
+    const [tasks, setTasks] = useState([]);
+    const [taskDescrition, setTaskDescrition] = useState("");
+
+    const handleTaskDescriptionChange = (event) => {
+        setTaskDescrition(event.target.value);
+    }
+
+    const handleAddTask = () => {
+        setTasks([taskDescrition, ...tasks /* ... (spread - itération) */]);
+        setTaskDescrition("");
+    }
+
+
     return (
         <fieldset>
             <legend>Task Manager</legend>
-            <input />
-            <button>Ajouter une tâche</button>
+            <input placeholder="Texte de la tâche" onChange={handleTaskDescriptionChange}/>
+            <button onClick={handleAddTask}>Ajouter une tâche</button>
             <hr/>
-            <p></p>
+            <p>Aucune tâche pour le moment.</p>
             <ol>
                 <li>
                     <span>Tâche 1</span>
