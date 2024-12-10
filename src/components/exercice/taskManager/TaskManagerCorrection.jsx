2244f31bab4db39f79daf5549868b94d079075f1
Marie Delavalette <marie.delavalette@inetum.com>
2024-12-10 15:16:16 +0100

    ajout de la correction

---
+++ src/components/exercice/taskManager/TaskManagerCorrection.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+
+function TaskManagerCorrection() {
+
+    const [tasks, setTasks] = useState([]);
+    const [taskDescrition, setTaskDescription] = useState("");
+
+    const handleTaskDescriptionChange = (event) => {
+        setTaskDescription(event.target.value);
+    }
+
+    const handleAddTask = () => {
+        setTasks([taskDescrition, ...tasks /* ... (spread - itération) */]);
+        setTaskDescription("");
+    }
+
+    const handleDeleteTask = (task) => {
+        if (!task) return;
+        setTasks(tasks.filter(t => t !== task));
+    }
+
+
+    return (
+        <fieldset>
+            <legend>Task Manager</legend>
+            <input
+                placeholder="Texte de la tâche"
+                value={taskDescrition}
+                onChange={handleTaskDescriptionChange}
+            />
+            <button onClick={handleAddTask}>Ajouter une tâche</button>
+            <hr />
+            {
+                !tasks.length
+                    ?
+                    (<p>Aucune tâche pour le moment.</p>)
+                    :
+                    (
+                        <ol>
+                            {
+                                tasks.map((task, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <span>{task}</span>
+                                            <button onClick={() => handleDeleteTask(task)}>Supprimer</button>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ol>
+                    )
+            }
+        </fieldset>
+    )
+}
+
+export default TaskManagerCorrection;
