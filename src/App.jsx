4bbcf4b340effcebbb9fde0c101ab3acb8757589
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-12-10 14:23:57 +0100

    Préparation Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import TheoryWrapper from "./components/theory/TheoryWrapper"
+import TaskManager from "./components/exercice/TaskManager"
 
 
 function App() {
   return (
     <>
       <Header/>
+      <TaskManager/>
       {/* <TheoryWrapper/> */}
       <Footer/>
     </>
---
+++ src/components/exercice/TaskManager.jsx
@@ -0,0 +1,23 @@
+
+
+
+function TaskManager(){
+
+    return (
+        <fieldset>
+            <legend>Task Manager</legend>
+            <input />
+            <button>Ajouter une tâche</button>
+            <hr/>
+            <p></p>
+            <ol>
+                <li>
+                    <span>Tâche 1</span>
+                    <button>Supprimer</button>
+                </li>
+            </ol>
+        </fieldset>
+    )
+}
+
+export default TaskManager;
